6023f4bf0ad5253ca6bc75f5e334b4f2478a9ac3
AminaVenton <aaventon@gmail.com>
2024-06-12 17:19:49 +0000

    removed hardcoded paths

--- src/SalesPersonLookUp.java
+++ src/SalesPersonLookUp.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class SalesRepLookUp {
+public class SalesPersonLookUp {
     public static void main(String[] args) throws IOException {
 
         String salesRepFilePath = "SalesReps.csv";
